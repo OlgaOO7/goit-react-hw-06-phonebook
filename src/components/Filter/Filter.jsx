796fc31410c866e4b19dbc83aaad7ccec4cc8084
Olga007 <o.piankova@ukr.net>
2023-05-24 00:23:07 +0300

    filt chan

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,9 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { changeFilter, getFilter } from '../redux/filterSlice';
+import { useDispatch } from 'react-redux';
+import { changeFilter } from '../redux/filterSlice';
 import css from './Filter.module.css';
 
 export const Filter = () => {
   const dispatch = useDispatch();
-  // const filter = useSelector(getFilter);
   const handleFilterChange = evt => dispatch(changeFilter(evt.target.value));
 
   return (
@@ -13,7 +12,6 @@ export const Filter = () => {
       <input
         type="text"
         name="filter"
-        // value={filter}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
